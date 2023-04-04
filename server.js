4c73f70c03eab45836a7b9ee33777894f8f83a11
Akthiha <akthiha@gmail.com>
2023-04-04 17:08:33 +1000

    resolve

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const db = require("./db/connect");
 const Stations = require("./models/stations");
 const Owners = require("./models/owners");
-const Random = require("./models/random");
-const Stats = require("./models/stats");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -11,9 +9,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index", { apiKey: process.env.MAPS_API_KEY, commodityKey: process.env.COMMODITIES_API_KEY });
+app.get("/", async (req, res) => {
+  const statsData = await Stats.returnStats(); // call the returnStats function to get the data
+  res.render("index", { apiKey: process.env.MAPS_API_KEY, stats: statsData });
 });
+
+
 
 app.get("/api/stations/all", async (req, res) => {
   const data = await Stations.all();
@@ -25,14 +26,7 @@ app.get("/api/owners", async (req, res) => {
   res.json(data);
 });
 
-app.get(`/api/stations/random`, async (req, res) => {
-  const data = await Random.random();
-  res.json(data)
-})
-app.get("/api/stats", async (req, res) => {
-  const data = await Stats.returnStats();
-  res.json(data);
-});
+const Stats = require("./models/stats");
 
 app.get(`/keys/commodity`, async (req, res) => {
   res.json({commodityKey: process.env.COMMODITIES_API_KEY})
